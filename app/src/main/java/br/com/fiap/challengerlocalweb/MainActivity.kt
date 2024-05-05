ec1fdafc58c32fb934984126398baef35986ce41
Hebert Zachi <hebertzachi.contato@outlook.com>
2024-05-04 21:01:00 -0300

    :sparkles: feat: implementing nativagation and initial files project structure

--- app/src/main/java/br/com/fiap/challengerlocalweb/MainActivity.kt
+++ app/src/main/java/br/com/fiap/challengerlocalweb/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.fiap.challengerlocalweb.pages.calendar
+import br.com.fiap.challengerlocalweb.pages.emailCompose
+import br.com.fiap.challengerlocalweb.pages.emailViewer
+import br.com.fiap.challengerlocalweb.pages.inbox
+import br.com.fiap.challengerlocalweb.pages.sentItems
 import br.com.fiap.challengerlocalweb.ui.theme.ChallengerLocalWebTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +27,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "inbox"){
+                            composable(route = "inbox") { inbox(navController) }
+                            composable(route = "sentItems") { sentItems(navController) }
+                            composable(route = "emailCompose") { emailCompose(navController) }
+                            composable(route = "emailViewer") { emailViewer(navController) }
+                            composable(route = "calendar") { calendar(navController) }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChallengerLocalWebTheme {
-        Greeting("Android")
     }
 }
