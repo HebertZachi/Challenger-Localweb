e9b1a23ab8073c29150f2c39153d9939f0c52794
Rafael Lopes <rafael.lopesdesigner@gmail.com>
2024-05-06 22:17:29 -0300

    caixa de entrada

--- app/src/main/java/br/com/fiap/challengerlocalweb/pages/inbox.kt
+++ app/src/main/java/br/com/fiap/challengerlocalweb/pages/inbox.kt
@@ -1,12 +1,85 @@
 package br.com.fiap.challengerlocalweb.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun inbox(
     navController: NavController,
     ) {
-Text(text = "Teste")
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF253645))
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+        ) {
+            OutlinedTextField(
+
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                shape = RoundedCornerShape(50.dp),
+                placeholder = {
+                    Text(
+                        text = "Search",
+                        color = Color.White,
+                    )
+                }
+            )
+
+            Text(
+                text = "Caixa de entrada",
+                modifier = Modifier.padding(vertical = 10.dp),
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 22.sp,
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color.Transparent),
+                shape = RoundedCornerShape(0.dp),
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column {
+                        Text(text = "Teste 1")
+                    }
+                    Column {
+                        Text(text = "Felipe Rocha")
+                    }
+                    Column {
+                        Text(text = "08:00")
+                    }
+                }
+            }
+        }
+
+    }
 }
